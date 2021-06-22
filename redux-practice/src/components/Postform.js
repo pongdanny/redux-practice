ddf68a5e87714a786ddf9b5d6c79655cf0f55da3
danny <pongdaniel17@gmail.com>
2021-06-21 20:41:51 -0400

    add post

---
+++ redux-practice/src/components/Postform.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+            </div>
+        )
+    }
+}
+
+export default PostForm;
