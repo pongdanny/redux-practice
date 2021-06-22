568b9e86dc8b962b207226823ab80e868a7888d0
danny <pongdaniel17@gmail.com>
2021-06-21 21:06:35 -0400

    redux thunk

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Posts from "./components/Posts"
 import PostForm from "./components/Postform"
+import { Provider } from "react-redux"
+
+import store from "./store"
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +20,7 @@ class App extends Component {
       <hr />
       <Posts />
       </div>
+      </Provider>
     );
   }
 }
